f380c454470e721610107529254c78fe06c56734
Thikkavarapu Manikanta Reddy <t.manikantareddy160@gmail.com>
2023-02-17 17:21:29 -0500

    Changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload 7oo7.
+          Edit <code>src/App.js</code> and save to reload ooops.
         </p>
         <a
           className="App-link"
